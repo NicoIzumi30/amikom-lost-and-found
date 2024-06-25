194d9ea18b72135c5f64ab9789de20fe0dfa4d77
service500 <service500@localhost.localdomain>
2024-06-26 04:05:31 +0700

    Backend : add announcement

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,17 @@ use App\Http\Controllers;
 |
 */
 
-Route::get('/',Controllers\DashboardController::class)->name('administrator.dashboard');
+Route::get('/', Controllers\DashboardController::class)->name('administrator.dashboard');
 Route::prefix('administrator')->name('administrator.')->group(function () {
-    Route::get('login',[Controllers\Administrator\LoginController::class,'index'])->name('login');
-    Route::get('employees',[Controllers\Administrator\EmployeeController::class,'index'])->name('employees');
-    Route::get('students',[Controllers\Administrator\StudentController::class,'index'])->name('students');
-    Route::get('lost-items',[Controllers\Administrator\LostItemController::class,'index'])->name('lostItems');
-    Route::get('item-found',[Controllers\Administrator\ItemFoundController::class,'index'])->name('itemFound');
-    Route::get('announcement',[Controllers\Administrator\AnnouncementController::class,'index'])->name('announcement');
-    Route::get('get-started',[Controllers\Administrator\GetStartedController::class,'index'])->name('getStarted');
-    Route::get('profile',[Controllers\Administrator\ProfileController::class,'index'])->name('profile');
-});
+    Route::get('login', [Controllers\Administrator\LoginController::class, 'index'])->name('login');
+    Route::get('employees', [Controllers\Administrator\EmployeeController::class, 'index'])->name('employees');
+    Route::get('students', [Controllers\Administrator\StudentController::class, 'index'])->name('students');
+    Route::get('lost-items', [Controllers\Administrator\LostItemController::class, 'index'])->name('lostItems');
+    Route::get('item-found', [Controllers\Administrator\ItemFoundController::class, 'index'])->name('itemFound');
+    Route::get('announcement', [Controllers\Administrator\AnnouncementController::class, 'index'])->name('announcement');
 
+    Route::post('add', [Controllers\Administrator\AnnouncementController::class, 'store'])->name('store');
+
+    Route::get('get-started', [Controllers\Administrator\GetStartedController::class, 'index'])->name('getStarted');
+    Route::get('profile', [Controllers\Administrator\ProfileController::class, 'index'])->name('profile');
+});
