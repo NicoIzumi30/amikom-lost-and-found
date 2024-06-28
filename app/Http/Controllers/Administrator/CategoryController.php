d0e5adb37e1b1c54498fb79f2363dd2d9de408e0
unzurna <unzurna@students.amikom.ac.id>
2024-06-28 22:54:24 +0700

    create add category

--- app/Http/Controllers/Administrator/CategoryController.php
+++ app/Http/Controllers/Administrator/CategoryController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\Administrator;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view("administrator.category.index");
-    } 
+    }
+
+    public function store()
+    {
+
+    }
 }
